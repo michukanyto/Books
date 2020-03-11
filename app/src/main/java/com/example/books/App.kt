b9803735289ddcb0e3ffdca89e8a316b9049a1fd
michukanyto <escobar.marlon@gmail.com>
2020-03-10 22:07:30 -0400

    Timber implementation

--- app/src/main/java/com/example/books/App.kt
+++ app/src/main/java/com/example/books/App.kt
@@ -5,6 +5,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.books.database.AppDataBase
 import com.example.books.database.DATABASE_NAME
+import timber.log.Timber
 
 class App: Application() {
     companion object {
@@ -13,6 +14,7 @@ class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
         //Create DB
         dataBase = Room.databaseBuilder(this,
         AppDataBase::class.java,
