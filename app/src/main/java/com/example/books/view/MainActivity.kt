9fa93b9a8f3423c27672a7e5c47d204a74b62b11
michukanyto <escobar.marlon@gmail.com>
2020-03-16 21:55:20 -0400

    item_book_layout.xml created and swipeRefresh and ViewRecycler created in MainActivity

--- app/src/main/java/com/example/books/view/MainActivity.kt
+++ app/src/main/java/com/example/books/view/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.books.R
 
-class BookActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
