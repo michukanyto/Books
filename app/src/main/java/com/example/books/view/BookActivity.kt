c38e31740b2db1be4d4670c17cfc18c6fe3b5b5c
michukanyto <escobar.marlon@gmail.com>
2020-03-16 21:46:17 -0400

    BookViewModel.kt created

--- app/src/main/java/com/example/books/view/BookActivity.kt
+++ app/src/main/java/com/example/books/view/BookActivity.kt
@@ -1,9 +1,10 @@
-package com.example.books
+package com.example.books.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.books.R
 
-class MainActivity : AppCompatActivity() {
+class BookActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/books/view/BookViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.books.view
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.books.App
+import com.example.books.model.Book
+
+class BookViewModel: ViewModel() {
+
+    val books: LiveData<List<Book>> = App.dataBase.bookDao().getAllBooks()
+}
